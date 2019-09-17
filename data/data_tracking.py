5e384e270a7914ea42c8c032d11445b60398f1ba
William Jamieson <wjamieson@huskers.unl.edu>
2019-09-17 15:33:59 -0500

    Updated tracking setup data

--- data/data_tracking.py
+++ data/data_tracking.py
@@ -1,18 +1,37 @@
 import source.keyword as keyword
 
 
-genotype_attr = (keyword.genotype_keys, False)
+genotype_attr = (keyword.genotype,
+                 keyword.genotype_keys,
+                 False)
 genotype      = {keyword.genotype: genotype_attr}
 
-genotype_attrs = {keyword.egg:    genotype,
-                  keyword.larva:  genotype,
-                  keyword.pupa:   genotype,
-                  keyword.female: genotype}
+genotype_attrs = {agent_key: genotype
+                    for agent_key in keyword.insect_keys}
 
-death_attr = (keyword.death_keys, True)
+death_attr = (keyword.death,
+              keyword.death_keys,
+              True)
 death      = {keyword.death: death_attr}
 
-genotype_death = {**genotype, **death}
+death_attrs = {agent_key: death
+                  for agent_key in keyword.insect_keys}
 
+death_filter_attr = (keyword.death,
+                     keyword.death_keys,
+                     {keyword.genotype:
+                          (keyword.genotype_keys,
+                           True)
+                      })
+death_filter = {keyword.death_track: death_filter_attr}
+
+death_filter_attrs = {agent_key: death_filter
+                        for agent_key in keyword.insect_keys}
+
+genotype_death       = {**genotype, **death}
 genotype_death_attrs = {agent_key: genotype_death
                             for agent_key in keyword.insect_keys}
+
+genotype_death_filters = {**genotype, **death_filter}
+genotype_death_filters_attrs = {agent_key: genotype_death_filters
+                                    for agent_key in keyword.insect_keys}
